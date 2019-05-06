841931ce0523befdb5ff30a53c3b57ccc5919f59
Paulda07 <40849148+Paulda07@users.noreply.github.com>
2019-05-06 12:30:51 +0530

    Minor_Changes

--- Todo_App/todo/views.py
+++ Todo_App/todo/views.py
@@ -24,3 +24,6 @@ def index(request): #the index view
                 todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
                 todo.delete() #deleting todo
     return render(request, "index.html", {"todos": todos, "categories":categories})
+
+
+    
